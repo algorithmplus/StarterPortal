16cdc3ca0c604c85017d37d90a53a89380352c0e
jovin-joy <jovin.joy@education.gov.uk>
2020-04-14 19:00:23 +0100

    NSA-2971

--- pre-prod/orgs-db/002_NSA-2971_Financial returns project script_pp.sql
+++ pre-prod/orgs-db/002_NSA-2971_Financial returns project script_pp.sql
@@ -1,33 +1,40 @@
 BEGIN TRAN ADDORSPP
 DECLARE @organisationId uniqueidentifier
+DECLARE @legacyId uniqueidentifier
 -- New Gibson MAT Large
 SET @organisationId = NEWID()
+SET @legacyId = SELECT NEXT VALUE FOR org_legacy_id_sequence;
 INSERT INTO organisation(id, name, Category, Status, legacyId, companyRegistrationNumber, createdAt, updatedAt)
-VALUES(@organisationId, 'Gibson MAT Large', '013', 1, (SELECT NEXT VALUE FOR org_legacy_id_sequence AS numId), '77001500', GETDATE(), GETDATE());
+VALUES(@organisationId, 'Gibson MAT Large', '013', 1, @legacyId, '77001500', GETDATE(), GETDATE());
 
 -- New Gibson MAT Small
 SET @organisationId = NEWID()
+SET @legacyId = SELECT NEXT VALUE FOR org_legacy_id_sequence;
 INSERT INTO organisation(id, name, Category, Status, legacyId, companyRegistrationNumber, createdAt, updatedAt)
-VALUES(@organisationId, 'Gibson MAT Small', '013', 1, (SELECT NEXT VALUE FOR org_legacy_id_sequence AS numId), '77000400', GETDATE(), GETDATE());
+VALUES(@organisationId, 'Gibson MAT Small', '013', 1, @legacyId, '77000400', GETDATE(), GETDATE());
 
 -- New Gibson SAT
 SET @organisationId = NEWID()
+SET @legacyId = SELECT NEXT VALUE FOR org_legacy_id_sequence;
 INSERT INTO organisation(id, name, Category, Status, legacyId, companyRegistrationNumber, createdAt, updatedAt)
-VALUES(@organisationId, 'Gibson SAT', '010', 1, (SELECT NEXT VALUE FOR org_legacy_id_sequence AS numId), '77000100', GETDATE(), GETDATE());
+VALUES(@organisationId, 'Gibson SAT', '010', 1, @legacyId, '77000100', GETDATE(), GETDATE());
 
 -- New Aldgate MAT Large
 SET @organisationId = NEWID()
+SET @legacyId = SELECT NEXT VALUE FOR org_legacy_id_sequence;
 INSERT INTO organisation(id, name, Category, Status, legacyId, companyRegistrationNumber, createdAt, updatedAt)
-VALUES(@organisationId, 'Aldgate MAT Large', '013', 1, (SELECT NEXT VALUE FOR org_legacy_id_sequence AS numId), '88001500', GETDATE(), GETDATE());
+VALUES(@organisationId, 'Aldgate MAT Large', '013', 1, @legacyId, '88001500', GETDATE(), GETDATE());
 
 -- New Aldgate MAT Small
 SET @organisationId = NEWID()
+SET @legacyId = SELECT NEXT VALUE FOR org_legacy_id_sequence;
 INSERT INTO organisation(id, name, Category, Status, legacyId, companyRegistrationNumber, createdAt, updatedAt)
-VALUES(@organisationId, 'Aldgate MAT Small', '013', 1, (SELECT NEXT VALUE FOR org_legacy_id_sequence AS numId), '88000400', GETDATE(), GETDATE());
+VALUES(@organisationId, 'Aldgate MAT Small', '013', 1, @legacyId, '88000400', GETDATE(), GETDATE());
 
 -- New Aldgate SAT
 SET @organisationId = NEWID()
+SET @legacyId = SELECT NEXT VALUE FOR org_legacy_id_sequence;
 INSERT INTO organisation(id, name, Category, Status, legacyId, companyRegistrationNumber, createdAt, updatedAt)
-VALUES(@organisationId, 'Aldgate SAT', '010', 1, (SELECT NEXT VALUE FOR org_legacy_id_sequence AS numId), '88000100', GETDATE(), GETDATE());
+VALUES(@organisationId, 'Aldgate SAT', '010', 1, @legacyId, '88000100', GETDATE(), GETDATE());
 
 ROLLBACK TRAN ADDORSPP
