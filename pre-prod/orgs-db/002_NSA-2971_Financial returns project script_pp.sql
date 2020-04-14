fd5132938f4407b151180fa9fcb56fb540a015e0
jovin-joy <jovin.joy@education.gov.uk>
2020-04-14 17:45:27 +0100

    NSA-2971

--- pre-prod/orgs-db/002_NSA-2971_Financial returns project script_pp.sql
+++ pre-prod/orgs-db/002_NSA-2971_Financial returns project script_pp.sql
@@ -3,31 +3,31 @@ DECLARE @organisationId uniqueidentifier
 -- New Gibson MAT Large
 SET @organisationId = NEWID()
 INSERT INTO organisation(id, name, Category, Status, legacyId, companyRegistrationNumber, createdAt, updatedAt)
-VALUES(@organisationId, 'Gibson MAT Large', '013', 1, (select next_value from counters where counter_name = 'organisation_legacyid'), '77001500', GETDATE(), GETDATE());
-update counters set next_value = (select next_value + 1 from counters where counter_name = 'organisation_legacyid') where counter_name = 'organisation_legacyid';
+VALUES(@organisationId, 'Gibson MAT Large', '013', 1, (SELECT NEXT VALUE FOR org_legacy_id_sequence AS numId), '77001500', GETDATE(), GETDATE());
+
 -- New Gibson MAT Small
 SET @organisationId = NEWID()
 INSERT INTO organisation(id, name, Category, Status, legacyId, companyRegistrationNumber, createdAt, updatedAt)
-VALUES(@organisationId, 'Gibson MAT Small', '013', 1, (select next_value from counters where counter_name = 'organisation_legacyid'), '77000400', GETDATE(), GETDATE());
-update counters set next_value = (select next_value + 1 from counters where counter_name = 'organisation_legacyid') where counter_name = 'organisation_legacyid';
+VALUES(@organisationId, 'Gibson MAT Small', '013', 1, (SELECT NEXT VALUE FOR org_legacy_id_sequence AS numId), '77000400', GETDATE(), GETDATE());
+
 -- New Gibson SAT
 SET @organisationId = NEWID()
 INSERT INTO organisation(id, name, Category, Status, legacyId, companyRegistrationNumber, createdAt, updatedAt)
-VALUES(@organisationId, 'Gibson SAT', '010', 1, (select next_value from counters where counter_name = 'organisation_legacyid'), '77000100', GETDATE(), GETDATE());
-update counters set next_value = (select next_value + 1 from counters where counter_name = 'organisation_legacyid') where counter_name = 'organisation_legacyid';
+VALUES(@organisationId, 'Gibson SAT', '010', 1, (SELECT NEXT VALUE FOR org_legacy_id_sequence AS numId), '77000100', GETDATE(), GETDATE());
+
 -- New Aldgate MAT Large
 SET @organisationId = NEWID()
 INSERT INTO organisation(id, name, Category, Status, legacyId, companyRegistrationNumber, createdAt, updatedAt)
-VALUES(@organisationId, 'Aldgate MAT Large', '013', 1, (select next_value from counters where counter_name = 'organisation_legacyid'), '88001500', GETDATE(), GETDATE());
-update counters set next_value = (select next_value + 1 from counters where counter_name = 'organisation_legacyid') where counter_name = 'organisation_legacyid';
+VALUES(@organisationId, 'Aldgate MAT Large', '013', 1, (SELECT NEXT VALUE FOR org_legacy_id_sequence AS numId), '88001500', GETDATE(), GETDATE());
+
 -- New Aldgate MAT Small
 SET @organisationId = NEWID()
 INSERT INTO organisation(id, name, Category, Status, legacyId, companyRegistrationNumber, createdAt, updatedAt)
-VALUES(@organisationId, 'Aldgate MAT Small', '013', 1, (select next_value from counters where counter_name = 'organisation_legacyid'), '88000400', GETDATE(), GETDATE());
-update counters set next_value = (select next_value + 1 from counters where counter_name = 'organisation_legacyid') where counter_name = 'organisation_legacyid';
+VALUES(@organisationId, 'Aldgate MAT Small', '013', 1, (SELECT NEXT VALUE FOR org_legacy_id_sequence AS numId), '88000400', GETDATE(), GETDATE());
+
 -- New Aldgate SAT
 SET @organisationId = NEWID()
 INSERT INTO organisation(id, name, Category, Status, legacyId, companyRegistrationNumber, createdAt, updatedAt)
-VALUES(@organisationId, 'Aldgate SAT', '010', 1, (select next_value from counters where counter_name = 'organisation_legacyid'), '88000100', GETDATE(), GETDATE());
-update counters set next_value = (select next_value + 1 from counters where counter_name = 'organisation_legacyid') where counter_name = 'organisation_legacyid';
+VALUES(@organisationId, 'Aldgate SAT', '010', 1, (SELECT NEXT VALUE FOR org_legacy_id_sequence AS numId), '88000100', GETDATE(), GETDATE());
+
 ROLLBACK TRAN ADDORSPP
