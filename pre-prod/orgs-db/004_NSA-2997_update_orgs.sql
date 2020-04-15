4af26665fe4e18e4741fee527b13013641e9786f
Steve Coleman-Williams <steve@archronos.com>
2020-04-15 20:25:57 +0100

    added the update script

---
+++ pre-prod/orgs-db/004_NSA-2997_update_orgs.sql
@@ -0,0 +1,202 @@
+BEGIN TRAN ADDROATPORGS
+UPDATE 
+    organisation
+SET 
+    UKPRN = '10066783',
+    STATUS = 1
+WHERE
+    id = 'a24f16fe-ed1d-42ee-b236-062fd7f7301f';
+
+UPDATE 
+    organisation
+SET 
+    UKPRN = '10082452',
+    STATUS = 1
+WHERE
+    id = '72efea6e-81a3-494e-95e4-0ef4be66ccde';
+
+UPDATE 
+    organisation
+SET 
+    UKPRN = '10084461',
+    STATUS = 1
+WHERE
+    id = '6357165f-bb5f-4663-a9dd-1301f765158a';
+
+UPDATE 
+    organisation
+SET 
+    UKPRN = '10085306',
+    STATUS = 1
+WHERE
+    id = 'be2c623f-2fb1-45d7-a010-1eaf6938dfd8';
+
+UPDATE 
+    organisation
+SET 
+    UKPRN = '10084420',
+    STATUS = 1
+WHERE
+    id = '1890a754-b0ae-4a21-b842-24b7554467fb';
+
+UPDATE 
+    organisation
+SET 
+    UKPRN = '10064431',
+    STATUS = 1
+WHERE
+    id = 'ac728c4f-3c4e-4455-8210-2e1fae672358';
+
+UPDATE 
+    organisation
+SET 
+    UKPRN = '10061558',
+    STATUS = 1
+WHERE
+    id = 'dbea3267-863f-4041-bd8f-3fed09fa6652';
+
+UPDATE 
+    organisation
+SET 
+    UKPRN = '10082269',
+    STATUS = 1
+WHERE
+    id = 'b179ebd7-9cba-4597-b8c9-4bec4aaa6bcd';
+
+UPDATE 
+    organisation
+SET 
+    UKPRN = '10081176',
+    STATUS = 1
+WHERE
+    id = 'c7b7b5a6-3bd4-4510-8795-6dbd097be930';
+
+UPDATE 
+    organisation
+SET 
+    UKPRN = '10063806',
+    STATUS = 1
+WHERE
+    id = 'e21c86f0-b27a-4640-9ba6-83b477277beb';
+
+UPDATE 
+    organisation
+SET 
+    UKPRN = '10084918',
+    STATUS = 1
+WHERE
+    id = 'f1df15c9-5331-4d49-b25c-94ac29404aea';
+
+UPDATE 
+    organisation
+SET 
+    UKPRN = '10048199',
+    STATUS = 1
+WHERE
+    id = '3a24eb93-2199-4d7c-8332-a1f0de979ac1';
+
+UPDATE 
+    organisation
+SET 
+    UKPRN = '10084572',
+    STATUS = 1
+WHERE
+    id = 'ebeab65e-823d-4534-84e0-abca94a07b2f';
+
+UPDATE 
+    organisation
+SET 
+    UKPRN = '10068386',
+    STATUS = 1
+WHERE
+    id = '7b23b277-a5ef-4e25-b2ef-b1d162a01828';
+
+UPDATE 
+    organisation
+SET 
+    UKPRN = '10083474',
+    STATUS = 1
+WHERE
+    id = 'c6504f4f-5581-4554-aff9-b96b9120f518';
+
+UPDATE 
+    organisation
+SET 
+    UKPRN = '10056336',
+    STATUS = 1
+WHERE
+    id = 'babac9f0-a320-486a-8ca4-bd9f14be51ae';
+
+UPDATE 
+    organisation
+SET 
+    UKPRN = '10065010',
+    STATUS = 1
+WHERE
+    id = 'd2263fa0-4b4d-408f-9b0e-cb53b97f33a1';
+
+UPDATE 
+    organisation
+SET 
+    UKPRN = '10085493',
+    STATUS = 1
+WHERE
+    id = 'c6b43d87-b5c6-4f41-ab37-cc45d0023ae2';
+
+UPDATE 
+    organisation
+SET 
+    UKPRN = '10062398',
+    STATUS = 1
+WHERE
+    id = 'c9764427-15fc-4eb3-af8f-cea94619ab1c';
+
+UPDATE 
+    organisation
+SET 
+    UKPRN = '10082827',
+    STATUS = 1
+WHERE
+    id = '724344e1-c942-4228-879a-d4c76c507985';
+
+UPDATE 
+    organisation
+SET 
+    UKPRN = '10035178',
+    STATUS = 1
+WHERE
+    id = '2f12e12e-99a8-4a07-8d0f-d856a930b7fb';
+
+UPDATE 
+    organisation
+SET 
+    UKPRN = '10084937',
+    STATUS = 1
+WHERE
+    id = '83231855-141e-47d2-982f-e3a9ed91b94e';
+
+UPDATE 
+    organisation
+SET 
+    UKPRN = '10047502',
+    STATUS = 1
+WHERE
+    id = '0b47e007-3acc-490a-ad86-eaa601d2adf9';
+
+UPDATE 
+    organisation
+SET 
+    UKPRN = '10084785',
+    STATUS = 1
+WHERE
+    id = 'abddc009-6473-4929-a255-f79b81996fa0';
+
+UPDATE 
+    organisation
+SET 
+    UKPRN = '10082311',
+    STATUS = 1
+WHERE
+    id = 'bcca0b7a-9e03-43ab-86fb-fd3039362a58';
+
+ROLLBACK TRAN ADDROATPORGS
