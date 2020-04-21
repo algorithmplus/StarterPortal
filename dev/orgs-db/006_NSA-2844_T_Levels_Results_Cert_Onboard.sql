963894e8d588b72b0af8a11e106185ad8481cfc4
Jose Luis Garcia Ramos <joseluisgraa@gmail.com>
2020-04-21 11:08:19 +0100

    Using sequence and setting org ids at the top

--- dev/orgs-db/006_NSA-2844_T_Levels_Results_Cert_Onboard.sql
+++ dev/orgs-db/006_NSA-2844_T_Levels_Results_Cert_Onboard.sql
@@ -4,6 +4,11 @@ DECLARE @roleId UNIQUEIDENTIFIER;
 DECLARE @policyId UNIQUEIDENTIFIER;
 DECLARE @serviceId UNIQUEIDENTIFIER;
 DECLARE @orgId UNIQUEIDENTIFIER;
+DECLARE @numericId BIGINT;
+
+DECLARE @ncfeOrgId UNIQUEIDENTIFIER = '1c058b23-1d46-422e-bc6d-34a04d7a7e7d';
+DECLARE @pearsonOrgId UNIQUEIDENTIFIER = 'b79a7d27-c62c-48b1-8ef7-fd16b8e737a9';
+DECLARE @cityGuildsOrgId UNIQUEIDENTIFIER = '7e9e54e6-3524-4e3a-b467-2c2fa12c732b';
 
 --Create service
 --TO DO obtain redirectUrl and post logout url
@@ -26,30 +31,33 @@ VALUES (@policyId, 'T Levels and Certification', @serviceId, 1, GETDATE(), GETDA
 
 --Create Policy Conditions
 INSERT INTO PolicyCondition (Id, PolicyId, Field, Operator, Value, CreatedAt, UpdatedAt)
-VALUES (NEWID(), @policyId, 'organisation.id', 'is', '1c058b23-1d46-422e-bc6d-34a04d7a7e7d', GETDATE(), GETDATE()),
-        (NEWID(), @policyId, 'organisation.id', 'is', 'b79a7d27-c62c-48b1-8ef7-fd16b8e737a9', GETDATE(), GETDATE()),
-        (NEWID(), @policyId, 'organisation.id', 'is', '7e9e54e6-3524-4e3a-b467-2c2fa12c732b', GETDATE(), GETDATE());
+VALUES (NEWID(), @policyId, 'organisation.id', 'is', @ncfeOrgId, GETDATE(), GETDATE()),
+        (NEWID(), @policyId, 'organisation.id', 'is', @pearsonOrgId, GETDATE(), GETDATE()),
+        (NEWID(), @policyId, 'organisation.id', 'is', @cityGuildsOrgId, GETDATE(), GETDATE());
 
 --Create Role 'Site Administrator' and link to policy
 SET @roleId = NEWID();
+SET @numericId = NEXT VALUE FOR role_numeric_id_sequence;
 INSERT INTO Role (Id, Name, ApplicationId, Status, CreatedAt, UpdatedAt, Code, NumericId, ParentId)
-VALUES (@roleId, 'Site Administrator', NEWID(), 1, GETDATE(), GETDATE(), 't_levels_site_admin', (SELECT MAX(NumericId) FROM Role) + 1, null);
+VALUES (@roleId, 'Site Administrator', NEWID(), 1, GETDATE(), GETDATE(), 't_levels_site_admin', @numericId, null);
 
 INSERT INTO PolicyRole (PolicyId, RoleId, CreatedAt, UpdatedAt)
 VALUES (@policyId, @roleId, GETDATE(), GETDATE());
 
 --Create Role 'T Levels Reviewer' and link to policy
 SET @roleId = NEWID();
+SET @numericId = NEXT VALUE FOR role_numeric_id_sequence;
 INSERT INTO Role (Id, Name, ApplicationId, Status, CreatedAt, UpdatedAt, Code, NumericId, ParentId)
-VALUES (@roleId, 'T Levels Reviewer', @serviceId, 1, GETDATE(), GETDATE(), 't_levels_reviewer', (SELECT MAX(NumericId) FROM Role) + 1, null);
+VALUES (@roleId, 'T Levels Reviewer', @serviceId, 1, GETDATE(), GETDATE(), 't_levels_reviewer', @numericId, null);
 
 INSERT INTO PolicyRole (PolicyId, RoleId, CreatedAt, UpdatedAt)
 VALUES (@policyId, @roleId, GETDATE(), GETDATE());
 
 --Create Role 'Providers Editor' and link to policy
 SET @roleId = NEWID();
+SET @numericId = NEXT VALUE FOR role_numeric_id_sequence;
 INSERT INTO Role (Id, Name, ApplicationId, Status, CreatedAt, UpdatedAt, Code, NumericId, ParentId)
-VALUES (@roleId, 'Providers Editor', @serviceId, 1, GETDATE(), GETDATE(), 't_levels_providers_editor', (SELECT MAX(NumericId) FROM Role) + 1, null);
+VALUES (@roleId, 'Providers Editor', @serviceId, 1, GETDATE(), GETDATE(), 't_levels_providers_editor', @numericId, null);
 
 INSERT INTO PolicyRole (PolicyId, RoleId, CreatedAt, UpdatedAt)
 VALUES (@policyId, @roleId, GETDATE(), GETDATE());
--- prod/orgs-db/008_NSA-2844_T_Levels_Results_Cert_Onboard.sql
+++ prod/orgs-db/008_NSA-2844_T_Levels_Results_Cert_Onboard.sql
@@ -4,6 +4,11 @@ DECLARE @roleId UNIQUEIDENTIFIER;
 DECLARE @policyId UNIQUEIDENTIFIER;
 DECLARE @serviceId UNIQUEIDENTIFIER;
 DECLARE @orgId UNIQUEIDENTIFIER;
+DECLARE @numericId BIGINT;
+
+DECLARE @ncfeOrgId UNIQUEIDENTIFIER = '72ed2dcb-7fa3-456c-9618-21f52c314d5';
+DECLARE @pearsonOrgId UNIQUEIDENTIFIER = 'be17ddce-c8fd-45ce-b4b4-4a98faebc445';
+DECLARE @cityGuildsOrgId UNIQUEIDENTIFIER = '180b2f48-129d-4400-a2e4-1aa820d8ba08';
 
 --Create service
 --TO DO obtain redirectUrl and post logout url
@@ -26,30 +31,33 @@ VALUES (@policyId, 'T Levels and Certification', @serviceId, 1, GETDATE(), GETDA
 
 --Create Policy Conditions
 INSERT INTO PolicyCondition (Id, PolicyId, Field, Operator, Value, CreatedAt, UpdatedAt)
-VALUES (NEWID(), @policyId, 'organisation.id', 'is', '72ed2dcb-7fa3-456c-9618-21f52c314d5', GETDATE(), GETDATE()),
-        (NEWID(), @policyId, 'organisation.id', 'is', 'be17ddce-c8fd-45ce-b4b4-4a98faebc445', GETDATE(), GETDATE()),
-        (NEWID(), @policyId, 'organisation.id', 'is', '180b2f48-129d-4400-a2e4-1aa820d8ba08', GETDATE(), GETDATE());
+VALUES (NEWID(), @policyId, 'organisation.id', 'is', @ncfeOrgId, GETDATE(), GETDATE()),
+        (NEWID(), @policyId, 'organisation.id', 'is', @pearsonOrgId, GETDATE(), GETDATE()),
+        (NEWID(), @policyId, 'organisation.id', 'is', @cityGuildsOrgId, GETDATE(), GETDATE());
 
 --Create Role 'Site Administrator' and link to policy
 SET @roleId = NEWID();
+SET @numericId = NEXT VALUE FOR role_numeric_id_sequence;
 INSERT INTO Role (Id, Name, ApplicationId, Status, CreatedAt, UpdatedAt, Code, NumericId, ParentId)
-VALUES (@roleId, 'Site Administrator', NEWID(), 1, GETDATE(), GETDATE(), 't_levels_site_admin', (SELECT MAX(NumericId) FROM Role) + 1, null);
+VALUES (@roleId, 'Site Administrator', NEWID(), 1, GETDATE(), GETDATE(), 't_levels_site_admin', @numericId, null);
 
 INSERT INTO PolicyRole (PolicyId, RoleId, CreatedAt, UpdatedAt)
 VALUES (@policyId, @roleId, GETDATE(), GETDATE());
 
 --Create Role 'T Levels Reviewer' and link to policy
 SET @roleId = NEWID();
+SET @numericId = NEXT VALUE FOR role_numeric_id_sequence;
 INSERT INTO Role (Id, Name, ApplicationId, Status, CreatedAt, UpdatedAt, Code, NumericId, ParentId)
-VALUES (@roleId, 'T Levels Reviewer', @serviceId, 1, GETDATE(), GETDATE(), 't_levels_reviewer', (SELECT MAX(NumericId) FROM Role) + 1, null);
+VALUES (@roleId, 'T Levels Reviewer', @serviceId, 1, GETDATE(), GETDATE(), 't_levels_reviewer', @numericId, null);
 
 INSERT INTO PolicyRole (PolicyId, RoleId, CreatedAt, UpdatedAt)
 VALUES (@policyId, @roleId, GETDATE(), GETDATE());
 
 --Create Role 'Providers Editor' and link to policy
 SET @roleId = NEWID();
+SET @numericId = NEXT VALUE FOR role_numeric_id_sequence;
 INSERT INTO Role (Id, Name, ApplicationId, Status, CreatedAt, UpdatedAt, Code, NumericId, ParentId)
-VALUES (@roleId, 'Providers Editor', @serviceId, 1, GETDATE(), GETDATE(), 't_levels_providers_editor', (SELECT MAX(NumericId) FROM Role) + 1, null);
+VALUES (@roleId, 'Providers Editor', @serviceId, 1, GETDATE(), GETDATE(), 't_levels_providers_editor', @numericId, null);
 
 INSERT INTO PolicyRole (PolicyId, RoleId, CreatedAt, UpdatedAt)
 VALUES (@policyId, @roleId, GETDATE(), GETDATE());
