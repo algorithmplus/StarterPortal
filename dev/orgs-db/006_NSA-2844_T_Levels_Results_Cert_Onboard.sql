a4b70551cfb75ca71fb76254706b38e93379e8d6
Jose Luis Garcia Ramos <joseluisgraa@gmail.com>
2020-04-22 09:25:04 +0100

    Added error handling showing error details

--- dev/orgs-db/006_NSA-2844_T_Levels_Results_Cert_Onboard.sql
+++ dev/orgs-db/006_NSA-2844_T_Levels_Results_Cert_Onboard.sql
@@ -132,6 +132,15 @@ BEGIN TRY
 END TRY
 
 BEGIN CATCH
+
+     SELECT   
+        ERROR_NUMBER() AS ErrorNumber  
+        ,ERROR_SEVERITY() AS ErrorSeverity  
+        ,ERROR_STATE() AS ErrorState  
+        ,ERROR_PROCEDURE() AS ErrorProcedure  
+        ,ERROR_LINE() AS ErrorLine  
+        ,ERROR_MESSAGE() AS ErrorMessage;  
+
      --Rollback if there was an error
      IF @@TRANCOUNT > 0
           ROLLBACK TRAN TLEVELSRESULTSCERTONBOARD;
--- prod/orgs-db/008_NSA-2844_T_Levels_Results_Cert_Onboard.sql
+++ prod/orgs-db/008_NSA-2844_T_Levels_Results_Cert_Onboard.sql
@@ -1,127 +1,148 @@
 BEGIN TRAN TLEVELSRESULTSCERTONBOARD
 
-DECLARE @roleId UNIQUEIDENTIFIER;
-DECLARE @policyId UNIQUEIDENTIFIER;
-DECLARE @serviceId UNIQUEIDENTIFIER;
-DECLARE @orgId UNIQUEIDENTIFIER;
-DECLARE @manageId UNIQUEIDENTIFIER;
-DECLARE @numericId BIGINT;
+BEGIN TRY
 
-DECLARE @ncfeOrgId UNIQUEIDENTIFIER = '72ed2dcb-7fa3-456c-9618-21f52c314d5';
-DECLARE @pearsonOrgId UNIQUEIDENTIFIER = 'be17ddce-c8fd-45ce-b4b4-4a98faebc445';
-DECLARE @cityGuildsOrgId UNIQUEIDENTIFIER = '180b2f48-129d-4400-a2e4-1aa820d8ba08';
+     DECLARE @roleId UNIQUEIDENTIFIER;
+     DECLARE @policyId UNIQUEIDENTIFIER;
+     DECLARE @serviceId UNIQUEIDENTIFIER;
+     DECLARE @orgId UNIQUEIDENTIFIER;
+     DECLARE @manageId UNIQUEIDENTIFIER;
+     DECLARE @numericId BIGINT;
 
---Create service
-SET @serviceId = NEWID();
-INSERT INTO service (id, name, description, clientId, clientSecret, apiSecret, tokenEndpointAuthMethod, serviceHome, postResetUrl, isExternalService, isMigrated, parentId, isChildService)
-VALUES (@serviceId, 'T Level Results and Certification', 'T Level Results and Certification', 'TLevelsRC', NULL, NULL, 'client_secret_post', 'https://manage-tlevel-results.tlevels.gov.uk/Dashboard', NULL, 1, 0, NULL, 0);
-INSERT INTO serviceRedirectUris (serviceId, redirectUrl)
-VALUES (@serviceId, 'https://manage-tlevel-results.tlevels.gov.uk/auth/cb');
-INSERT INTO servicePostLogoutRedirectUris (serviceId, redirectUrl)
-VALUES (@serviceId, 'https://manage-tlevel-results.tlevels.gov.uk/signout/complete');
-INSERT INTO serviceParams (serviceId, paramName, paramValue)
-VALUES (@serviceId, 'allowManageInvite', 'true');
-INSERT INTO serviceGrantTypes (serviceId, grantType)
-VALUES (@serviceId, 'authorization_code');
+     DECLARE @ncfeOrgId UNIQUEIDENTIFIER = '72ed2dcb-7fa3-456c-9618-21f52c314d5';
+     DECLARE @pearsonOrgId UNIQUEIDENTIFIER = 'be17ddce-c8fd-45ce-b4b4-4a98faebc445';
+     DECLARE @cityGuildsOrgId UNIQUEIDENTIFIER = '180b2f48-129d-4400-a2e4-1aa820d8ba08';
 
---Create Policy
-SET @policyId = NEWID();
-INSERT INTO Policy (Id, Name, ApplicationId, Status, CreatedAt, UpdatedAt)
-VALUES (@policyId, 'T Levels and Certification', @serviceId, 1, GETDATE(), GETDATE());
+     --Create service
+     SET @serviceId = NEWID();
+     INSERT INTO service (id, name, description, clientId, clientSecret, apiSecret, tokenEndpointAuthMethod, serviceHome, postResetUrl, isExternalService, isMigrated, parentId, isChildService)
+     VALUES (@serviceId, 'T Level Results and Certification', 'T Level Results and Certification', 'TLevelsRC', NULL, NULL, 'client_secret_post', 'https://manage-tlevel-results.tlevels.gov.uk/Dashboard', NULL, 1, 0, NULL, 0);
+     INSERT INTO serviceRedirectUris (serviceId, redirectUrl)
+     VALUES (@serviceId, 'https://manage-tlevel-results.tlevels.gov.uk/auth/cb');
+     INSERT INTO servicePostLogoutRedirectUris (serviceId, redirectUrl)
+     VALUES (@serviceId, 'https://manage-tlevel-results.tlevels.gov.uk/signout/complete');
+     INSERT INTO serviceParams (serviceId, paramName, paramValue)
+     VALUES (@serviceId, 'allowManageInvite', 'true');
+     INSERT INTO serviceGrantTypes (serviceId, grantType)
+     VALUES (@serviceId, 'authorization_code');
 
---Create Policy Conditions
-INSERT INTO PolicyCondition (Id, PolicyId, Field, Operator, Value, CreatedAt, UpdatedAt)
-VALUES (NEWID(), @policyId, 'organisation.id', 'is', @ncfeOrgId, GETDATE(), GETDATE()),
-        (NEWID(), @policyId, 'organisation.id', 'is', @pearsonOrgId, GETDATE(), GETDATE()),
-        (NEWID(), @policyId, 'organisation.id', 'is', @cityGuildsOrgId, GETDATE(), GETDATE());
+     --Create Policy
+     SET @policyId = NEWID();
+     INSERT INTO Policy (Id, Name, ApplicationId, Status, CreatedAt, UpdatedAt)
+     VALUES (@policyId, 'T Levels and Certification', @serviceId, 1, GETDATE(), GETDATE());
 
---Create Role 'Site Administrator' and link to policy
-SET @roleId = NEWID();
-SET @numericId = NEXT VALUE FOR role_numeric_id_sequence;
-INSERT INTO Role (Id, Name, ApplicationId, Status, CreatedAt, UpdatedAt, Code, NumericId, ParentId)
-VALUES (@roleId, 'Site Administrator', @serviceId, 1, GETDATE(), GETDATE(), 't_levels_site_administrator', @numericId, null);
+     --Create Policy Conditions
+     INSERT INTO PolicyCondition (Id, PolicyId, Field, Operator, Value, CreatedAt, UpdatedAt)
+     VALUES (NEWID(), @policyId, 'organisation.id', 'is', @ncfeOrgId, GETDATE(), GETDATE()),
+          (NEWID(), @policyId, 'organisation.id', 'is', @pearsonOrgId, GETDATE(), GETDATE()),
+          (NEWID(), @policyId, 'organisation.id', 'is', @cityGuildsOrgId, GETDATE(), GETDATE());
 
-INSERT INTO PolicyRole (PolicyId, RoleId, CreatedAt, UpdatedAt)
-VALUES (@policyId, @roleId, GETDATE(), GETDATE());
+     --Create Role 'Site Administrator' and link to policy
+     SET @roleId = NEWID();
+     SET @numericId = NEXT VALUE FOR role_numeric_id_sequence;
+     INSERT INTO Role (Id, Name, ApplicationId, Status, CreatedAt, UpdatedAt, Code, NumericId, ParentId)
+     VALUES (@roleId, 'Site Administrator', @serviceId, 1, GETDATE(), GETDATE(), 't_levels_site_administrator', @numericId, null);
 
---Create Role 'T Levels Reviewer' and link to policy
-SET @roleId = NEWID();
-SET @numericId = NEXT VALUE FOR role_numeric_id_sequence;
-INSERT INTO Role (Id, Name, ApplicationId, Status, CreatedAt, UpdatedAt, Code, NumericId, ParentId)
-VALUES (@roleId, 'T Levels Reviewer', @serviceId, 1, GETDATE(), GETDATE(), 't_levels_reviewer', @numericId, null);
+     INSERT INTO PolicyRole (PolicyId, RoleId, CreatedAt, UpdatedAt)
+     VALUES (@policyId, @roleId, GETDATE(), GETDATE());
 
-INSERT INTO PolicyRole (PolicyId, RoleId, CreatedAt, UpdatedAt)
-VALUES (@policyId, @roleId, GETDATE(), GETDATE());
+     --Create Role 'T Levels Reviewer' and link to policy
+     SET @roleId = NEWID();
+     SET @numericId = NEXT VALUE FOR role_numeric_id_sequence;
+     INSERT INTO Role (Id, Name, ApplicationId, Status, CreatedAt, UpdatedAt, Code, NumericId, ParentId)
+     VALUES (@roleId, 'T Levels Reviewer', @serviceId, 1, GETDATE(), GETDATE(), 't_levels_reviewer', @numericId, null);
 
---Create Role 'Providers Editor' and link to policy
-SET @roleId = NEWID();
-SET @numericId = NEXT VALUE FOR role_numeric_id_sequence;
-INSERT INTO Role (Id, Name, ApplicationId, Status, CreatedAt, UpdatedAt, Code, NumericId, ParentId)
-VALUES (@roleId, 'Providers Editor', @serviceId, 1, GETDATE(), GETDATE(), 't_levels_providers_editor', @numericId, null);
+     INSERT INTO PolicyRole (PolicyId, RoleId, CreatedAt, UpdatedAt)
+     VALUES (@policyId, @roleId, GETDATE(), GETDATE());
 
-INSERT INTO PolicyRole (PolicyId, RoleId, CreatedAt, UpdatedAt)
-VALUES (@policyId, @roleId, GETDATE(), GETDATE());
+     --Create Role 'Providers Editor' and link to policy
+     SET @roleId = NEWID();
+     SET @numericId = NEXT VALUE FOR role_numeric_id_sequence;
+     INSERT INTO Role (Id, Name, ApplicationId, Status, CreatedAt, UpdatedAt, Code, NumericId, ParentId)
+     VALUES (@roleId, 'Providers Editor', @serviceId, 1, GETDATE(), GETDATE(), 't_levels_providers_editor', @numericId, null);
 
---Add Manage Roles
---service Config
-SET @manageId = (SELECT id FROM service WHERE clientId = 'manage')
-SET @numericId = NEXT VALUE FOR role_numeric_id_sequence;
-INSERT INTO Role
-(Id, Name, ApplicationId, CreatedAt, UpdatedAt, Code, NumericId, ParentId)
-SELECT NEWID(),
-     name + ' - Service Configuration',
-     @manageId,
-     GETDATE(),
-     GETDATE(),
-     UPPER(cast(id as varchar(40))) + '_serviceconfig',
-     @numericId,
-     NULL
-FROM service
-WHERE id = @serviceId
+     INSERT INTO PolicyRole (PolicyId, RoleId, CreatedAt, UpdatedAt)
+     VALUES (@policyId, @roleId, GETDATE(), GETDATE());
 
---service banner
-SET @numericId = NEXT VALUE FOR role_numeric_id_sequence;
-INSERT INTO Role
-(Id, Name, ApplicationId, CreatedAt, UpdatedAt, Code, NumericId, ParentId)
-SELECT NEWID(),
-     name + ' - Service Banner',
-     @manageId,
-     GETDATE(),
-     GETDATE(),
-     UPPER(cast(id as varchar(40))) + '_serviceBanner',
-     @numericId,
-     NULL
-FROM service
-WHERE id = @serviceId
+     --Add Manage Roles
+     --service Config
+     SET @manageId = (SELECT id FROM service WHERE clientId = 'manage')
+     SET @numericId = NEXT VALUE FOR role_numeric_id_sequence;
+     INSERT INTO Role
+     (Id, Name, ApplicationId, CreatedAt, UpdatedAt, Code, NumericId, ParentId)
+     SELECT NEWID(),
+          name + ' - Service Configuration',
+          @manageId,
+          GETDATE(),
+          GETDATE(),
+          UPPER(cast(id as varchar(40))) + '_serviceconfig',
+          @numericId,
+          NULL
+     FROM service
+     WHERE id = @serviceId
 
---service access management
-SET @numericId = NEXT VALUE FOR role_numeric_id_sequence;
-INSERT INTO Role
-(Id, Name, ApplicationId, CreatedAt, UpdatedAt, Code, NumericId, ParentId)
-SELECT NEWID(),
-     name + ' - Service Access Management',
-     @manageId,
-     GETDATE(),
-     GETDATE(),
-     UPPER(cast(id as varchar(40))) + '_accessManage',
-     @numericId,
-     NULL
-FROM service
-WHERE id = @serviceId
+     --service banner
+     SET @numericId = NEXT VALUE FOR role_numeric_id_sequence;
+     INSERT INTO Role
+     (Id, Name, ApplicationId, CreatedAt, UpdatedAt, Code, NumericId, ParentId)
+     SELECT NEWID(),
+          name + ' - Service Banner',
+          @manageId,
+          GETDATE(),
+          GETDATE(),
+          UPPER(cast(id as varchar(40))) + '_serviceBanner',
+          @numericId,
+          NULL
+     FROM service
+     WHERE id = @serviceId
 
---service support
-SET @numericId = NEXT VALUE FOR role_numeric_id_sequence;
-INSERT INTO Role
-(Id, Name, ApplicationId, CreatedAt, UpdatedAt, Code, NumericId, ParentId)
-SELECT NEWID(),
-     name + ' - Service Support',
-     @manageId,
-     GETDATE(),
-     GETDATE(),
-     UPPER(cast(id as varchar(40))) + '_serviceSup',
-     @numericId,
-     NULL
-FROM service
-WHERE id = @serviceId
+     --service access management
+     SET @numericId = NEXT VALUE FOR role_numeric_id_sequence;
+     INSERT INTO Role
+     (Id, Name, ApplicationId, CreatedAt, UpdatedAt, Code, NumericId, ParentId)
+     SELECT NEWID(),
+          name + ' - Service Access Management',
+          @manageId,
+          GETDATE(),
+          GETDATE(),
+          UPPER(cast(id as varchar(40))) + '_accessManage',
+          @numericId,
+          NULL
+     FROM service
+     WHERE id = @serviceId
 
-ROLLBACK TRAN TLEVELSRESULTSCERTONBOARD
+     --service support
+     SET @numericId = NEXT VALUE FOR role_numeric_id_sequence;
+     INSERT INTO Role
+     (Id, Name, ApplicationId, CreatedAt, UpdatedAt, Code, NumericId, ParentId)
+     SELECT NEWID(),
+          name + ' - Service Support',
+          @manageId,
+          GETDATE(),
+          GETDATE(),
+          UPPER(cast(id as varchar(40))) + '_serviceSup',
+          @numericId,
+          NULL
+     FROM service
+     WHERE id = @serviceId
+
+     --Commit transaction if all went fine
+     ROLLBACK TRAN TLEVELSRESULTSCERTONBOARD;
+
+END TRY
+
+BEGIN CATCH
+
+     SELECT   
+        ERROR_NUMBER() AS ErrorNumber  
+        ,ERROR_SEVERITY() AS ErrorSeverity  
+        ,ERROR_STATE() AS ErrorState  
+        ,ERROR_PROCEDURE() AS ErrorProcedure  
+        ,ERROR_LINE() AS ErrorLine  
+        ,ERROR_MESSAGE() AS ErrorMessage;  
+
+     --Rollback if there was an error
+     IF @@TRANCOUNT > 0
+          ROLLBACK TRAN TLEVELSRESULTSCERTONBOARD;
+
+END CATCH;
