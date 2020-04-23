0d3117011ec6af7012fa9402c946e42354bdb456
Steve Coleman-Williams <steve@archronos.com>
2020-04-23 16:12:20 +0100

    prod script

--- prod/orgs-db/008_NSA-2813_remove_asp_ofsted_download_role.sql
+++ prod/orgs-db/008_NSA-2813_remove_asp_ofsted_download_role.sql
@@ -3,19 +3,34 @@ BEGIN TRY
 
 IF (SELECT COUNT(*)
 FROM ROLE
-WHERE name = 'ASP Ofsted Download' AND ApplicationId = 'afeca973-433f-46bb-a3d6-163d6f522ae3')  = 1
+WHERE name = 'ASP Ofsted Download' AND ApplicationId = 'df2ae7f3-917a-4489-8a62-8b9b536a71cc')  = 1
 BEGIN
+    DELETE FROM 
+            PolicyCondition 
+        WHERE 
+            PolicyId = 'cfe830e6-85ac-4184-a3df-fb1a00d2f1e3';
+
+    DELETE FROM 
+            PolicyRole 
+        WHERE 
+            RoleId = 'afeca973-433f-46bb-a3d6-163d6f522ae3';
+
     DELETE FROM
             Role
         WHERE
             name = 'ASP Ofsted Download' AND
-        ApplicationId = 'afeca973-433f-46bb-a3d6-163d6f522ae3';
+        ApplicationId = 'df2ae7f3-917a-4489-8a62-8b9b536a71cc';
+
+    DELETE FROM 
+            Policy
+        WHERE
+            id = 'cfe830e6-85ac-4184-a3df-fb1a00d2f1e3';
 END
 ELSE
 BEGIN
     IF (SELECT COUNT(*)
     FROM ROLE
-    WHERE name = 'ASP Ofsted Download' AND ApplicationId = 'afeca973-433f-46bb-a3d6-163d6f522ae3') > 1
+    WHERE name = 'ASP Ofsted Download' AND ApplicationId = 'df2ae7f3-917a-4489-8a62-8b9b536a71cc') > 1
         PRINT 'The role `ASP Ofsted Download has duplicates';
     ELSE 
         PRINT 'The role `ASP Ofsted Download` does not exist'
