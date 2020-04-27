189e2e9cf7c22ab2f12283cf082c11ee9e315cad
Jose Luis Garcia Ramos <joseluisgraa@gmail.com>
2020-04-27 14:58:47 +0100

    Fix for ids

--- prod/orgs-db/010_NSA-3053_add_service_config_manage_user.sql
+++ prod/orgs-db/010_NSA-3053_add_service_config_manage_user.sql
@@ -2,21 +2,18 @@ BEGIN TRAN ADDSERVICECONFIGUSER
 
 BEGIN TRY
 
-  DECLARE @userId UNIQUEIDENTIFIER = '2b8145fc-00e6-44cd-9de5-3d1905e30767';
-  DECLARE @orgId UNIQUEIDENTIFIER = '7d48d8ff-a322-4f22-8a79-c0425ac69749';
-  DECLARE @serviceId UNIQUEIDENTIFIER = 'b1f190aa-729a-45fc-a695-4ea209dc79d4';
   DECLARE @roleName VARCHAR(500) = 'T Level Results and Certification - Service Configuration';
   DECLARE @roleId UNIQUEIDENTIFIER;
 
-      INSERT INTO user_services (id, status, user_id, organisation_id, service_id, createdAt, updatedAt)
-      VALUES (NEWID(), 1, @userId,@orgId, @serviceId, GETDATE(), GETDATE());
+    INSERT INTO user_services (id, status, user_id, organisation_id, service_id, createdAt, updatedAt)
+    VALUES (NEWID(), 1, '2b8145fc-00e6-44cd-9de5-3d1905e30767', '7d48d8ff-a322-4f22-8a79-c0425ac69749', 'b1f190aa-729a-45fc-a695-4ea209dc79d4', GETDATE(), GETDATE());
 
     SET @roleId = (SELECT id FROM Role WHERE name = @roleName);
 
     IF (@roleId IS NOT NULL)
         BEGIN
             INSERT INTO user_service_roles (id, user_id, service_id, organisation_id, role_id, createdAt, updatedAt)
-            VALUES (NEWID(), @userId, @serviceId, @orgId, @roleId, GETDATE(), GETDATE());
+            VALUES (NEWID(), '2b8145fc-00e6-44cd-9de5-3d1905e30767', 'b1f190aa-729a-45fc-a695-4ea209dc79d4', '7d48d8ff-a322-4f22-8a79-c0425ac69749', @roleId, GETDATE(), GETDATE());
         END
 
   ROLLBACK TRAN ADDSERVICECONFIGUSER
