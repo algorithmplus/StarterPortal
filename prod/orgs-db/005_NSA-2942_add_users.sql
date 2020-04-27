37381c280dd179c8b7c1f1375aae6bc46274924e
D A - Work <deka.adan@education.gov.uk>
2020-04-27 09:27:28 +0100

    removed duplicate lines

--- prod/orgs-db/005_NSA-2942_add_users.sql
+++ prod/orgs-db/005_NSA-2942_add_users.sql
@@ -6,7 +6,6 @@ BEGIN TRY
     DECLARE @serviceId UNIQUEIDENTIFIER = 'b1f190aa-729a-45fc-a695-4ea209dc79d4';
     DECLARE @roleId UNIQUEIDENTIFIER;
 
-    --Teacher Services - Employer Access - Agent
     --INSERT into User Services
         INSERT INTO user_services (id, status, user_id, organisation_id, service_id, createdAt, updatedAt)
         VALUES (NEWID(), 1,'ec8d1e57-9645-4fa8-a360-c790944c056e', @orgId, @serviceId, GETDATE(), GETDATE());
@@ -17,6 +16,7 @@ BEGIN TRY
         INSERT INTO user_services (id, status, user_id, organisation_id, service_id, createdAt, updatedAt)
         VALUES (NEWID(), 1,'7ea0a96c-986c-4f8b-abd9-942d344ed5e9', @orgId, @serviceId, GETDATE(), GETDATE());
 
+    --Teacher Services - Employer Access - Agent
     --INSERT into User Services Roles
     SET @roleId = '40b35632-0510-4440-9e9c-adecab61d31e';
         INSERT INTO user_service_roles (id, user_id, service_id, organisation_id, role_id, createdAt, updatedAt)
@@ -29,16 +29,6 @@ BEGIN TRY
         VALUES (NEWID(), '7ea0a96c-986c-4f8b-abd9-942d344ed5e9', @serviceId, @orgId, @roleId, GETDATE(), GETDATE());
 
     --Teacher Services - Employer Access - Schools
-    --INSERT into User Services
-        INSERT INTO user_services (id, status, user_id, organisation_id, service_id, createdAt, updatedAt)
-        VALUES (NEWID(), 1,'ec8d1e57-9645-4fa8-a360-c790944c056e',@orgId, @serviceId, GETDATE(), GETDATE());
-        INSERT INTO user_services (id, status, user_id, organisation_id, service_id, createdAt, updatedAt)
-        VALUES (NEWID(), 1,'bbf3261c-9c9c-46a8-bdea-c86fd63391a2',@orgId, @serviceId, GETDATE(), GETDATE());
-        INSERT INTO user_services (id, status, user_id, organisation_id, service_id, createdAt, updatedAt)
-        VALUES (NEWID(), 1,'fb9c75a9-f163-4b9f-af3d-6b2f06af3db4',@orgId, @serviceId, GETDATE(), GETDATE());
-        INSERT INTO user_services (id, status, user_id, organisation_id, service_id, createdAt, updatedAt)
-        VALUES (NEWID(), 1,'7ea0a96c-986c-4f8b-abd9-942d344ed5e9',@orgId, @serviceId, GETDATE(), GETDATE());
-
     --INSERT into User Services Roles
     SET @roleId = '5b78be3d-6c78-4c5a-9ed4-05465dd9e379';
         INSERT INTO user_service_roles (id, user_id, service_id, organisation_id, role_id, createdAt, updatedAt)
@@ -51,16 +41,6 @@ BEGIN TRY
         VALUES (NEWID(), '7ea0a96c-986c-4f8b-abd9-942d344ed5e9', @serviceId, @orgId, @roleId, GETDATE(), GETDATE());
 
     --Teacher Services - Appropriate Body
-    --INSERT into User Services
-        INSERT INTO user_services (id, status, user_id, organisation_id, service_id, createdAt, updatedAt)
-        VALUES (NEWID(), 1,'ec8d1e57-9645-4fa8-a360-c790944c056e',@orgId, @serviceId, GETDATE(), GETDATE());
-        INSERT INTO user_services (id, status, user_id, organisation_id, service_id, createdAt, updatedAt)
-        VALUES (NEWID(), 1,'bbf3261c-9c9c-46a8-bdea-c86fd63391a2',@orgId, @serviceId, GETDATE(), GETDATE());
-        INSERT INTO user_services (id, status, user_id, organisation_id, service_id, createdAt, updatedAt)
-        VALUES (NEWID(), 1,'fb9c75a9-f163-4b9f-af3d-6b2f06af3db4',@orgId, @serviceId, GETDATE(), GETDATE());
-        INSERT INTO user_services (id, status, user_id, organisation_id, service_id, createdAt, updatedAt)
-        VALUES (NEWID(), 1,'7ea0a96c-986c-4f8b-abd9-942d344ed5e9',@orgId, @serviceId, GETDATE(), GETDATE());
-
     --INSERT into User Services Roles
     SET @roleId = 'bb4ac13a-6ba4-48b3-973e-2e5f9589a6e3';
         INSERT INTO user_service_roles (id, user_id, service_id, organisation_id, role_id, createdAt, updatedAt)
@@ -73,16 +53,6 @@ BEGIN TRY
         VALUES (NEWID(), '7ea0a96c-986c-4f8b-abd9-942d344ed5e9', @serviceId, @orgId, @roleId, GETDATE(), GETDATE());
 
     --Teacher Services - ITT Provider
-    --INSERT into User Services
-        INSERT INTO user_services (id, status, user_id, organisation_id, service_id, createdAt, updatedAt)
-        VALUES (NEWID(), 1,'ec8d1e57-9645-4fa8-a360-c790944c056e',@orgId, @serviceId, GETDATE(), GETDATE());
-        INSERT INTO user_services (id, status, user_id, organisation_id, service_id, createdAt, updatedAt)
-        VALUES (NEWID(), 1,'bbf3261c-9c9c-46a8-bdea-c86fd63391a2',@orgId, @serviceId, GETDATE(), GETDATE());
-        INSERT INTO user_services (id, status, user_id, organisation_id, service_id, createdAt, updatedAt)
-        VALUES (NEWID(), 1,'fb9c75a9-f163-4b9f-af3d-6b2f06af3db4',@orgId, @serviceId, GETDATE(), GETDATE());
-        INSERT INTO user_services (id, status, user_id, organisation_id, service_id, createdAt, updatedAt)
-        VALUES (NEWID(), 1,'7ea0a96c-986c-4f8b-abd9-942d344ed5e9',@orgId, @serviceId, GETDATE(), GETDATE());
-
     --INSERT into User Services Roles
     SET @roleId = 'f620b42d-7f36-4175-985c-7761756a4749';
         INSERT INTO user_service_roles (id, user_id, service_id, organisation_id, role_id, createdAt, updatedAt)
