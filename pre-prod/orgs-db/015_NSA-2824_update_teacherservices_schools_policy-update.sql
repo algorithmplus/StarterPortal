aac78f3049307dfbbfc8bc1765aa92fc27154f07
D A - Work <deka.adan@education.gov.uk>
2020-04-29 16:46:10 +0100

    NSA-2824 - script to update HEI restriction for teacherservices schools policy

---
+++ pre-prod/orgs-db/015_NSA-2824_update_teacherservices_schools_policy-update.sql
@@ -0,0 +1,48 @@
+BEGIN TRAN TEACHERSERVICESPOLICYUPDATE
+
+BEGIN TRY
+
+    DECLARE @serviceName VARCHAR(500) = 'Teacher Services - Employer Access - Schools';
+    DECLARE @serviceId UNIQUEIDENTIFIER;
+    DECLARE @policyName VARCHAR(500) = 'TA Administration'
+    DECLARE @policyId UNIQUEIDENTIFIER;
+    DECLARE @PolicyCondition UNIQUEIDENTIFIER;
+
+      -- Get Service Id based on name, if more than one found it will fail and go to catch block
+      SET @serviceId = (SELECT id FROM Service WHERE name = @serviceName AND clientId = 'EvolveEmpAccessSchool');
+      IF (@serviceId IS NOT NULL)
+          BEGIN
+          -- Get Policy Id based on name, if more than one found it will fail and go to catch block
+          SET @policyId = (SELECT id FROM Policy WHERE ApplicationId = @serviceId AND Name = @policyName);
+          IF (@policyId IS NOT NULL)
+              BEGIN
+                  SET @policyConditionId = (SELECT id FROM PolicyCondition WHERE PolicyId = @policyId AND Field = 'organisation.type.id' AND VALUE = '29');
+                  IF (@policyConditionId IS NOT NULL)
+                      --  Update Policy Conditions
+                      UPDATE PolicyCondition
+                          SET Operator = '<>',
+                          UpdatedAt = GETDATE(),
+                          WHERE id = @policyConditionId;
+              END;
+          END;
+
+    -- COMMIT TRAN IF NO  ERRORS
+    ROLLBACK TRAN TEACHERSERVICESPOLICYUPDATE;
+
+END TRY
+
+BEGIN CATCH
+
+     SELECT
+        ERROR_NUMBER() AS ErrorNumber
+        ,ERROR_SEVERITY() AS ErrorSeverity
+        ,ERROR_STATE() AS ErrorState
+        ,ERROR_PROCEDURE() AS ErrorProcedure
+        ,ERROR_LINE() AS ErrorLine
+        ,ERROR_MESSAGE() AS ErrorMessage;
+
+     --Rollback if there was an error
+     IF @@TRANCOUNT > 0
+          ROLLBACK TRAN TEACHERSERVICESPOLICYUPDATE;
+
+END CATCH;
