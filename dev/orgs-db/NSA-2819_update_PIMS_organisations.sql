9729e204dacf3684fcff5afe0c15dae0a429cb45
Jose Luis Garcia Ramos <joseluisgraa@gmail.com>
2020-05-01 13:05:23 +0100

    New version that also UPDATES existing orgs

--- dev/orgs-db/NSA-2819_update_PIMS_organisations.sql
+++ dev/orgs-db/NSA-2819_update_PIMS_organisations.sql
@@ -35,59 +35,100 @@ BEGIN TRY
     --constant values
     DECLARE @orgCategory NVARCHAR(25) = '051';
 
-    --table where we store the orgs data that we are going to import
-    DECLARE @pimsOrgs AS TABLE (
+    --table where we store the orgs that we are going to INSERT (UKPRN not already present)
+    DECLARE @pimsOrgsInsert AS TABLE (
         UKPRN NVARCHAR(25),
         name NVARCHAR(500)
         );
 
-    --Get the data from temp table in the database (has to be previously loaded)
-    --We will load all UKPRNs that are not already present in the database
+    --Get UKPRN and name or all orgs that need to be INSERTED
     --using EXCEPT to compare only UKPRNs (as names might slightly differ)
-    INSERT INTO @pimsOrgs (UKPRN, name) 
-        SELECT UKPRN, name from pimsOrgsTemp
+    INSERT INTO @pimsOrgsInsert (UKPRN, name) 
+        SELECT UKPRN, name FROM pimsOrgsTemp
             WHERE UKPRN IN (
                 SELECT UKPRN   
                 FROM pimsOrgsTemp
                 EXCEPT  
                 SELECT UKPRN from organisation WHERE UKPRN IS NOT NULL);
 
+    --table where we store the orgs that need to be either INSERTED or UPDATED (not already present or present with different name)
+    DECLARE @pimsOrgsInsertAndUpdate AS TABLE (
+        UKPRN NVARCHAR(25),
+        name NVARCHAR(500)
+        );
+    INSERT INTO @pimsOrgsInsertAndUpdate (UKPRN, name) 
+        SELECT UKPRN, name FROM pimsOrgsTemp
+            EXCEPT  
+            SELECT UKPRN, Name from organisation WHERE UKPRN IS NOT NULL;
+
+    --table where we store the orgs that need to be UPDATED (present with different name)
+    DECLARE @pimsOrgsUpdate AS TABLE (
+        UKPRN NVARCHAR(25),
+        name NVARCHAR(500)
+        );
+    INSERT INTO @pimsOrgsUpdate (UKPRN, name) 
+        SELECT UKPRN, name FROM @pimsOrgsInsertAndUpdate
+            EXCEPT
+            SELECT UKPRN, name FROM @pimsOrgsInsert;
+
     --variables to store the org data to insert (one by one using cursor)
     DECLARE @orgUKPRN NVARCHAR(25);
     DECLARE @orgName NVARCHAR(500);
 
-    --counters for inserted and processed rows
+    --counters for inserted, updated and processed rows
     DECLARE @insertedRows INT = 0;
+    DECLARE @updatedRows INT = 0;
     DECLARE @processedRows INT = (SELECT COUNT(*) FROM pimsOrgsTemp);
     
-    --use cursor to loop through the list
-    DECLARE db_cursor_orgs CURSOR FOR SELECT UKPRN, Name FROM @pimsOrgs;
-    OPEN db_cursor_orgs;
-    FETCH NEXT FROM db_cursor_orgs INTO @orgUKPRN, @orgName;
+    --INSERT new Orgs
+    DECLARE db_cursor_orgs_insert CURSOR FOR SELECT UKPRN, Name FROM @pimsOrgsInsert;
+    OPEN db_cursor_orgs_insert;
+    FETCH NEXT FROM db_cursor_orgs_insert INTO @orgUKPRN, @orgName;
 
     WHILE @@FETCH_STATUS = 0
 
     BEGIN
 
-        IF NOT EXISTS (SELECT * FROM organisation WHERE UKPRN = @orgUKPRN)
-            BEGIN 
-                INSERT INTO organisation (id, name, category, ukprn, status, legacyId, createdAt, updatedAt)
-                    VALUES 
-                    (NEWID(), @orgName, @orgCategory, @orgUKPRN, 1, (NEXT VALUE FOR org_legacy_id_sequence), GETDATE(), GETDATE());
-                --increment counter of inserted rows
-                SET @insertedRows = @insertedRows + @@ROWCOUNT;
+        INSERT INTO organisation (id, name, category, ukprn, status, legacyId, createdAt, updatedAt)
+            VALUES 
+            (NEWID(), @orgName, @orgCategory, @orgUKPRN, 1, (NEXT VALUE FOR org_legacy_id_sequence), GETDATE(), GETDATE());
+        --increment counter of inserted rows
+        SET @insertedRows = @insertedRows + @@ROWCOUNT;
 
-                PRINT @orgName + ' added to organisations. UKPRN: ' + @orgUKPRN;
-            END
+        PRINT 'Org with UKPRN ' + @orgUKPRN + ' added. Name: ' + @orgName;
 
         -- get next org
-        FETCH NEXT FROM db_cursor_orgs INTO @orgUKPRN, @orgName;
+        FETCH NEXT FROM db_cursor_orgs_insert INTO @orgUKPRN, @orgName;
     END;
 
-    CLOSE db_cursor_orgs;
-    DEALLOCATE db_cursor_orgs;
+    CLOSE db_cursor_orgs_insert;
+    DEALLOCATE db_cursor_orgs_insert;
 
-    PRINT '*** Final Number of organisations added: ' + CAST(@insertedRows AS nvarchar(30));
+    --UPDATE existing Orgs
+    DECLARE db_cursor_orgs_update CURSOR FOR SELECT UKPRN, Name FROM @pimsOrgsUpdate;
+    OPEN db_cursor_orgs_update;
+    FETCH NEXT FROM db_cursor_orgs_update INTO @orgUKPRN, @orgName;
+
+    WHILE @@FETCH_STATUS = 0
+
+    BEGIN
+        
+        UPDATE organisation
+            SET name = @orgName , updatedAt = GETDATE() WHERE UKPRN = @orgUKPRN
+        --increment counter of updated rows
+        SET @updatedRows = @updatedRows + @@ROWCOUNT;
+
+        PRINT 'Org with UKPRN ' + @orgUKPRN + ' updated. New Name: ' + @orgName;
+
+        -- get next org
+        FETCH NEXT FROM db_cursor_orgs_update INTO @orgUKPRN, @orgName;
+    END;
+
+    CLOSE db_cursor_orgs_update;
+    DEALLOCATE db_cursor_orgs_update;
+
+    PRINT '*** Number of organisations added: ' + CAST(@insertedRows AS nvarchar(30));
+    PRINT '*** Number of organisations updated with new name: ' + CAST(@updatedRows AS nvarchar(30));
     PRINT '*** From a total of ' + CAST(@processedRows AS nvarchar(30)) + ' records processed.';
         
     --Commit transaction if all went fine
