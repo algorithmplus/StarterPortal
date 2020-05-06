0c7da7eecd1d8d426fa0de5dd938430201759141
D A - Work <deka.adan@education.gov.uk>
2020-05-06 15:03:09 +0100

    updated to user id instead of email

--- prod/orgs-db/NSA-3083_add_user_GIAS_backoffice_policy.sql
+++ prod/orgs-db/NSA-3083_add_user_GIAS_backoffice_policy.sql
@@ -8,12 +8,12 @@ BEGIN TRY
         SET @policyId_One = '88df18ad-7a0f-4617-ba76-13f48ae3149e';
             -- Create Policy Condition
             INSERT INTO PolicyCondition (Id, PolicyId, Field, Operator, Value, CreatedAt, UpdatedAt)
-            VALUES (NEWID(), @policyId_One, 'email', 'is', 'maggie.swinden@education.gov.uk', GETDATE(), GETDATE());
+            VALUES (NEWID(), @policyId_One, 'email', 'is', '679099df-be20-470f-bfa0-c281afba1b6a', GETDATE(), GETDATE());
 
         SET @policyId_Two = '46df98bb-3b86-4e0a-9ac9-e22c70243640';
               -- Create Policy Condition
             INSERT INTO PolicyCondition (Id, PolicyId, Field, Operator, Value, CreatedAt, UpdatedAt)
-            VALUES (NEWID(), @policyId_Two, 'email', 'is', 'maggie.swinden@education.gov.uk', GETDATE(), GETDATE());
+            VALUES (NEWID(), @policyId_Two, 'email', 'is', '679099df-be20-470f-bfa0-c281afba1b6a', GETDATE(), GETDATE());
 
     -- COMMIT TRAN IF NO ERRORS
     ROLLBACK TRAN ADDPUSERTOGIASBACKOFFICEPOLICY;
