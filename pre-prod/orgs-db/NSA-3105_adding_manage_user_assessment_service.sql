dac142ccc5bca9ae0c326169ded36e2a5c4a8dbf
D A - Work <deka.adan@education.gov.uk>
2020-05-19 13:14:41 +0100

    removed user_services entry

--- pre-prod/orgs-db/NSA-3105_adding_manage_user_assessment_service.sql
+++ pre-prod/orgs-db/NSA-3105_adding_manage_user_assessment_service.sql
@@ -7,9 +7,6 @@ DECLARE @orgId UNIQUEIDENTIFIER = '23f20e54-79ea-4146-8e39-18197576f023';
 DECLARE @serviceId UNIQUEIDENTIFIER = 'b1f190aa-729a-45fc-a695-4ea209dc79d4';
 DECLARE @roleId UNIQUEIDENTIFIER = '7e49d32b-40aa-48e2-91cf-926de537d723';
 
-    INSERT INTO user_services (id, status, user_id, organisation_id, service_id, createdAt, updatedAt)
-    VALUES (NEWID(), 1, @userId,@orgId, @serviceId, GETDATE(), GETDATE());
-
     INSERT INTO user_service_roles (id, user_id, service_id, organisation_id, role_id, createdAt, updatedAt)
     VALUES (NEWID(), @userId, @serviceId, @orgId, @roleId, GETDATE(), GETDATE());
 
