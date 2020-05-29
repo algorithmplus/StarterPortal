aeec0f930f8f2fa8b83513342e16f855ed2735ea
jovin-joy <jovin.joy@education.gov.uk>
2020-05-29 09:37:15 +0100

    NSA-3263 covid19 new form pp revert

---
+++ pre-prod/orgs-db/NSA-3263_Onboard_Covid-19_Schools_Fund-revert.sql
@@ -0,0 +1,44 @@
+BEGIN TRAN COVID19_SCHOOLS_FUND_FORM_REVERT
+
+BEGIN TRY
+
+ DECLARE @roleid UNIQUEIDENTIFIER;
+    DECLARE @policyid UNIQUEIDENTIFIER;
+    DECLARE @numericId BIGINT;
+    DECLARE @collectionServiceId UNIQUEIDENTIFIER = (select id from service where name ='Online Collections Service')
+    DECLARE @role1Id UNIQUEIDENTIFIER = (select Id from Role where Name='Coronavirus (COVID-19) Schools Fund' AND applicationId =@collectionServiceId)
+    DECLARE @policy1Id UNIQUEIDENTIFIER = (select Id from Policy where Name='COVID-19 Schools Fund - Orgs category' AND ApplicationId =@collectionServiceId)
+    DECLARE @policy2Id UNIQUEIDENTIFIER = (select Id from Policy where Name='COVID-19 Schools Fund - Orgs type' AND ApplicationId =@collectionServiceId)
+
+
+    DELETE from PolicyCondition where PolicyId=@policy1Id
+    DELETE from PolicyRole where PolicyId=@policy1Id AND RoleId=@role1Id
+    DELETE from Policy where Id=@policy1Id AND ApplicationId=@collectionServiceId
+
+    DELETE from PolicyCondition where PolicyId=@policy2Id
+    DELETE from PolicyRole where PolicyId=@policy2Id AND RoleId =@role1Id
+    DELETE from Policy where Id=@policy2Id AND ApplicationId =@collectionServiceId
+
+
+    DELETE from Role where Name='Coronavirus (COVID-19) Schools Fund' AND applicationId =@collectionServiceId
+    DELETE from serviceParams where paramName='COVID-19_SCHOOLS_FUND_FORM_ACCESS' AND serviceId =@collectionServiceId
+
+    ROLLBACK TRAN COVID19_SCHOOLS_FUND_FORM_REVERT
+
+END TRY
+
+BEGIN CATCH
+
+     SELECT
+        ERROR_NUMBER() AS ErrorNumber
+        ,ERROR_SEVERITY() AS ErrorSeverity
+        ,ERROR_STATE() AS ErrorState
+        ,ERROR_PROCEDURE() AS ErrorProcedure
+        ,ERROR_LINE() AS ErrorLine
+        ,ERROR_MESSAGE() AS ErrorMessage;
+
+     --Rollback if there was an error
+     IF @@TRANCOUNT > 0
+          ROLLBACK TRAN COVID19_SCHOOLS_FUND_FORM_REVERT;
+
+END CATCH;
