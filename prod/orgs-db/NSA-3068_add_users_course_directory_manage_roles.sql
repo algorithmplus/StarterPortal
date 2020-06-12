aefff8da8e4d4483a958e5b51724e97bac888526
D A - Work <deka.adan@education.gov.uk>
2020-06-12 10:45:34 +0100

    NSA-3068 - script to add manage users for Publish to the Course Directory in TEST, PRE-PROD and PROD environments

---
+++ prod/orgs-db/NSA-3068_add_users_course_directory_manage_roles.sql
@@ -0,0 +1,86 @@
+BEGIN TRAN ADDMANAGEROLEFORCD
+
+BEGIN TRY
+
+    DECLARE @userId_1 UNIQUEIDENTIFIER = '961a54f5-5521-438e-9c1c-ccef8593cc76';
+    DECLARE @userId_2 UNIQUEIDENTIFIER = '63d32a9d-46f0-4ad8-80f8-143c37dbc89f';
+    DECLARE @orgName VARCHAR(500) = 'Department for Education';
+    DECLARE @orgId UNIQUEIDENTIFIER;
+    DECLARE @serviceName VARCHAR(500) = 'DfE Sign-in manage';
+    DECLARE @serviceId UNIQUEIDENTIFIER;
+    DECLARE @roleName_1 VARCHAR(500) = 'Publish to the Course Directory - Service Access Management';
+    DECLARE @roleId_1 UNIQUEIDENTIFIER;
+    DECLARE @roleName_2 VARCHAR(500) = 'Publish to the Course Directory - Service Support';
+    DECLARE @roleId_2 UNIQUEIDENTIFIER;
+    DECLARE @roleName_3 VARCHAR(500) = 'Publish to the Course Directory - Service Banner';
+    DECLARE @roleId_3 UNIQUEIDENTIFIER;
+
+    -- Step 1) get organisation Id based on name, if more than one found it will fail and go to catch block
+    SET @orgId = (SELECT id FROM organisation WHERE name = @orgName);
+    IF (@orgId IS NOT NULL)
+        BEGIN
+
+        -- Step 1) get organisation Id based on name, if more than one found it will fail and go to catch block
+        SET @serviceId = (SELECT id FROM service WHERE name = @serviceName);
+        IF (@serviceId IS NOT NULL)
+            BEGIN
+
+            -- Step 1) get role Id based on name, if more than one found it will fail and go to catch block
+            SET @roleId_1 = (SELECT id FROM Role WHERE name = @roleName_1);
+            IF (@roleId_1 IS NOT NULL)
+
+                BEGIN
+
+                    INSERT INTO user_services (id, status, user_id, organisation_id, service_id, createdAt, updatedAt)
+                    VALUES  (NEWID(), 1, @userId_1, @orgId, @serviceId, GETDATE(), GETDATE());
+
+                    INSERT INTO user_service_roles (id, user_id, service_id, organisation_id, role_id, createdAt, updatedAt)
+                    VALUES  (NEWID(), @userId_1, @serviceId, @orgId, @roleId_1, GETDATE(), GETDATE()),
+                            (NEWID(), @userId_2, @serviceId, @orgId, @roleId_1, GETDATE(), GETDATE());
+
+                END;
+
+            SET @roleId_2 = (SELECT id FROM Role WHERE name = @roleName_2);
+            IF (@roleId_2 IS NOT NULL)
+
+                BEGIN
+
+                    INSERT INTO user_service_roles (id, user_id, service_id, organisation_id, role_id, createdAt, updatedAt)
+                    VALUES  (NEWID(), @userId_1, @serviceId, @orgId, @roleId_2, GETDATE(), GETDATE());
+
+                END;
+
+            SET @roleId_3 = (SELECT id FROM Role WHERE name = @roleName_3);
+            IF (@roleId_3 IS NOT NULL)
+
+                BEGIN
+
+                    INSERT INTO user_service_roles (id, user_id, service_id, organisation_id, role_id, createdAt, updatedAt)
+                    VALUES  (NEWID(), @userId_1, @serviceId, @orgId, @roleId_3, GETDATE(), GETDATE()),
+                            (NEWID(), @userId_2, @serviceId, @orgId, @roleId_3, GETDATE(), GETDATE());
+
+                END;
+
+            END;
+
+        END;
+
+    ROLLBACK TRAN ADDMANAGEROLEFORCD;
+
+END TRY
+
+BEGIN CATCH
+
+     SELECT
+        ERROR_NUMBER() AS ErrorNumber
+        ,ERROR_SEVERITY() AS ErrorSeverity
+        ,ERROR_STATE() AS ErrorState
+        ,ERROR_PROCEDURE() AS ErrorProcedure
+        ,ERROR_LINE() AS ErrorLine
+        ,ERROR_MESSAGE() AS ErrorMessage;
+
+     --Rollback if there was an error
+     IF @@TRANCOUNT > 0
+          ROLLBACK TRAN ADDMANAGEROLEFORCD;
+
+END CATCH;
