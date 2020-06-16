b75c79d4d34acc8db6e8cfc4861649846d47fa63
D A - Work <deka.adan@education.gov.uk>
2020-06-16 10:28:03 +0100

    updated script to link role to policy im

--- prod/orgs-db/NSA-3055_add_role_MTC_service.sql
+++ prod/orgs-db/NSA-3055_add_role_MTC_service.sql
@@ -4,8 +4,9 @@ BEGIN TRY
 
     DECLARE @serviceName VARCHAR(500) = 'Multiplication tables check';
     DECLARE @serviceId UNIQUEIDENTIFIER;
+    DECLARE @policyName VARCHAR(500) = 'MTC Internal user';
+    DECLARE @policyId UNIQUEIDENTIFIER;
     DECLARE @roleId UNIQUEIDENTIFIER;
-    DECLARE @applicationId UNIQUEIDENTIFIER;
     DECLARE @numericId BIGINT;
 
     -- Step 1) get service Id based on name, if more than one found it will fail and go to catch block
@@ -19,6 +20,19 @@ BEGIN TRY
         SET @numericId = NEXT VALUE FOR role_numeric_id_sequence;
             INSERT INTO Role (Id, Name, ApplicationId, Status, CreatedAt, UpdatedAt, Code, NumericId, ParentId)
             VALUES (@roleId, 'Technical Support', @serviceId, 1, GETDATE(), GETDATE(), 'mtc_tech_support', @numericId, NULL);
+
+        -- Step 2) get policy Id based on name, if more than one found it will fail and go to catch block
+        SET @policyId = (SELECT id FROM Policy WHERE Name = @policyName);
+        IF (@policyId IS NOT NULL)
+
+            BEGIN
+
+            --PolicyRole: Link Technical Support role for MTC service to MTC Internal roles policy
+
+                INSERT INTO PolicyRole (PolicyId, RoleId, CreatedAt, UpdatedAt)
+                VALUES (@policyId, @roleId, GETDATE(), GETDATE());
+
+            END;
 
         END;
 
