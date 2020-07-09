f03153cbb6365d74c8eb0d27e865af63d4cbd328
D A - Work <deka.adan@education.gov.uk>
2020-07-09 18:33:24 +0100

    NSA-3551 - GIAP Approvers script specific for TEST env

--- test/orgs-db/NSA-3551_add_approvers_GIAP_service.sql
+++ test/orgs-db/NSA-3551_add_approvers_GIAP_service.sql
@@ -1,120 +1,111 @@
 BEGIN TRAN GIAPAPPROVERS
-
 BEGIN TRY
 
+    DECLARE @serviceName_OLD VARCHAR(500) = 'Apply to convert';
+    DECLARE @serviceId_OLD UNIQUEIDENTIFIER;
+    DECLARE @serviceName_NEW VARCHAR(500) = 'Get information about pupils';
+    DECLARE @serviceId_NEW UNIQUEIDENTIFIER;
+    DECLARE @roleName VARCHAR(500) = 'GIAP Approver';
+    DECLARE @roleId UNIQUEIDENTIFIER;
     DECLARE @orgId UNIQUEIDENTIFIER;
-    DECLARE @serviceName_1 VARCHAR(500) = 'Key to Success (LAs)';
-    DECLARE @serviceName_2 VARCHAR(500) = 'Key to Success (Schools)';
-    DECLARE @serviceId_1 UNIQUEIDENTIFIER;
-    DECLARE @serviceId_2 UNIQUEIDENTIFIER;
-    DECLARE @userIdList AS TABLE (user_id NVARCHAR(500), organisation_id NVARCHAR(500));
     DECLARE @userId UNIQUEIDENTIFIER;
-    DECLARE @approverK2SIdList AS TABLE (user_id NVARCHAR(500), organisation_id NVARCHAR(500));
+    DECLARE @userIdList AS TABLE (user_id NVARCHAR(500), organisation_id NVARCHAR(500));
     DECLARE @approverK2SId UNIQUEIDENTIFIER;
-    DECLARE @approverMATSIdList AS TABLE (user_id NVARCHAR(500), organisation_id NVARCHAR(500));
+    DECLARE @approverK2SIdList AS TABLE (count NVARCHAR(500), user_id NVARCHAR(500), organisation_id NVARCHAR(500));
     DECLARE @approverMATSI UNIQUEIDENTIFIER;
+    DECLARE @approverMATSIdList AS TABLE (count NVARCHAR(500), user_id NVARCHAR(500), organisation_id NVARCHAR(500));
+
 
     --get service Id based on name, if more than one found it will fail and go to catch block
-    SET @serviceId_1 = (SELECT id FROM service WHERE name = @serviceName_1);
-    IF (@serviceId_1 IS NOT NULL)
+    SET @serviceId_NEW = (SELECT id FROM service WHERE name = @serviceName_NEW);
+    IF (@serviceId_NEW IS NOT NULL)
 
         BEGIN
 
-        SET @serviceId_2 = (SELECT id FROM service WHERE name = @serviceName_2);
-        IF (@serviceId_2 IS NOT NULL)
-
-            BEGIN
-                -- step 1) delete all policies (policy, policy conditions, and policyRoles) linked to the roles with that applicationId
-
-                INSERT INTO @userIdList (user_id, organisation_id) SELECT user_id, organisation_id FROM user_services WHERE service_id = @serviceId_1 OR service_id = @serviceId_2;
-
-                IF (SELECT COUNT (*) FROM @userIdList ) > 0
-                    BEGIN
-
-                    --use cursor to loop through the list
-                    DECLARE db_cursor CURSOR FOR SELECT user_id, organisation_id FROM @userIdList;
-                        OPEN db_cursor
-
-                    FETCH NEXT FROM db_cursor INTO @userId, @orgId
-
-                    END;
-
-                    CLOSE db_cursor;
-                    DEALLOCATE db_cursor;
-
-            END;
+        SET @serviceId_OLD = (SELECT id FROM service WHERE name = @serviceName_OLD);
+        IF (@serviceId_OLD IS NOT NULL)
 
             BEGIN
 
-                INSERT INTO @approverK2SIdList (user_id, organisation_id) SELECT user_id, organisation_id FROM user_organisation WHERE user_id = @userId AND role_id = '10000';
-                IF (SELECT COUNT (*) FROM @approverK2SIdList ) > 0
+            SET @roleId = (SELECT id FROM Role WHERE name = @roleName);
+            IF (@roleId IS NOT NULL)
 
-                    BEGIN
+                BEGIN
 
-                    --INSERT into appropriate columns
-                    DECLARE db_cursor CURSOR FOR SELECT user_id, organisation_id FROM @approverK2SIdList;
-                        OPEN db_cursor;
-
-                        FETCH NEXT FROM db_cursor INTO @userId, @orgId;
-
-                    WHILE @@FETCH_STATUS = 0
+                    INSERT INTO @approverK2SIdList (count, user_id, organisation_id) SELECT COUNT(1), us.user_id, us.organisation_id FROM [user_services] us INNER JOIN [user_organisation] uo  on us.user_id = uo.user_id  WHERE service_id = '03bc03c1-8661-4fcd-8ea0-70ee1b9585c8' AND role_id = '10000' GROUP BY us.user_id, us.organisation_id;
+                    IF (SELECT COUNT (*) FROM @approverK2SIdList ) > 0
 
                         BEGIN
 
-                        --INSERT into User Services
-                        IF NOT EXISTS (SELECT @userId, @orgId from @approverK2SIdList)
-                        INSERT INTO user_services (id, status, user_id, organisation_id, service_id, createdAt, updatedAt)
-                        VALUES  (NEWID(), 1, @userId, @orgId, @serviceId_1, GETDATE(), GETDATE()),
-                                (NEWID(), 1, @userId, @orgId, @serviceId_2, GETDATE(), GETDATE());
+                            DECLARE db_cursor CURSOR FOR SELECT user_id, organisation_id FROM @approverK2SIdList;
+                                OPEN db_cursor;
 
-                        FETCH NEXT FROM db_cursor INTO @userId, @orgId;
+                                FETCH NEXT FROM db_cursor INTO @userId, @orgId;
 
-                        END;
+                                WHILE @@FETCH_STATUS = 0
 
-                    CLOSE db_cursor;
-                    DEALLOCATE db_cursor;
+                                BEGIN
 
-                    END;
+                                --INSERT into User Services
+                                IF NOT EXISTS (SELECT @userId, @orgId from @approverK2SIdList)
+                                INSERT INTO user_services (id, status, user_id, organisation_id, service_id, createdAt, updatedAt)
+                                VALUES (NEWID(), 1, @userId, @orgId, @serviceId_NEW, GETDATE(), GETDATE());
 
-            END;
+                                INSERT INTO user_service_roles (id, user_id, service_id, organisation_id, role_id, createdAt, updatedAt)
+                                VALUES  (NEWID(), @userId, @serviceId_NEW, @orgId, @roleId, GETDATE(), GETDATE());
 
-            BEGIN
 
-                INSERT INTO @approverMATSIdList (user_id, organisation_id) SELECT uo.user_id as user_id, o.id as organistion_id FROM [organisation] o INNER JOIN [user_organisation] uo on organisation_id = o.id  WHERE Category = '010' AND role_id = '10000';
-                IF (SELECT COUNT (*) FROM @approverMATSIdList ) > 0
+                                FETCH NEXT FROM db_cursor INTO @userId, @orgId;
 
-                    BEGIN
-
-                    --INSERT into appropriate columns
-                    DECLARE db_cursor CURSOR FOR SELECT user_id, organisation_id FROM @approverMATSIdList;
-                        OPEN db_cursor;
-
-                    FETCH NEXT FROM db_cursor INTO @userId, @orgId;
-
-                    WHILE @@FETCH_STATUS = 0
-
-                        BEGIN
-
-                            --INSERT into User Services
-                            IF NOT EXISTS (SELECT @userId, @orgId from @approverMATSIdList)
-                            INSERT INTO user_services (id, status, user_id, organisation_id, service_id, createdAt, updatedAt)
-                            VALUES  (NEWID(), 1, @userId, @orgId, @serviceId_1, GETDATE(), GETDATE()),
-                                    (NEWID(), 1, @userId, @orgId, @serviceId_2, GETDATE(), GETDATE());
-
-                            FETCH NEXT FROM db_cursor INTO @userId, @orgId;
+                                END;
+                            CLOSE db_cursor;
+                            DEALLOCATE db_cursor;
 
                         END;
 
                     END;
 
-                    CLOSE db_cursor;
-                    DEALLOCATE db_cursor;
+                BEGIN
+
+                    INSERT INTO @approverMATSIdList (count, user_id, organisation_id) SELECT COUNT(1), uo.user_id, uo.organisation_id FROM [user_organisation] uo INNER JOIN [organisation] o on o.id = UO.organisation_id WHERE Category = '010' AND role_id = '10000' GROUP BY uo.user_id, uo.organisation_id;
+
+                    IF (SELECT COUNT (*) FROM @approverMATSIdList ) > 0
+
+                        BEGIN
+
+                            DECLARE db_cursor CURSOR FOR SELECT user_id, organisation_id FROM @approverMATSIdList;
+                                OPEN db_cursor;
+
+                                FETCH NEXT FROM db_cursor INTO @userId, @orgId;
+
+                                WHILE @@FETCH_STATUS = 0
+
+                                BEGIN
+
+                                --INSERT into User Services
+                                IF NOT EXISTS (SELECT @userId, @orgId from @approverMATSIdList)
+                                INSERT INTO user_services (id, status, user_id, organisation_id, service_id, createdAt, updatedAt)
+                                VALUES (NEWID(), 1, @userId, @orgId, @serviceId_NEW, GETDATE(), GETDATE());
+
+                                INSERT INTO user_service_roles (id, user_id, service_id, organisation_id, role_id, createdAt, updatedAt)
+                                VALUES  (NEWID(), @userId, @serviceId_NEW, @orgId, @roleId, GETDATE(), GETDATE());
+
+
+                                FETCH NEXT FROM db_cursor INTO @userId, @orgId;
+
+                                END;
+                            CLOSE db_cursor;
+                            DEALLOCATE db_cursor;
+
+                        END;
+
+                    END;
 
             END;
 
         END;
 
-    ROLLBACK TRAN GIAPAPPROVERS;
+    COMMIT TRAN GIAPAPPROVERS;
 
 END TRY
 
@@ -127,8 +118,8 @@ BEGIN CATCH
         ,ERROR_PROCEDURE() AS ErrorProcedure
         ,ERROR_LINE() AS ErrorLine
         ,ERROR_MESSAGE() AS ErrorMessage;
-
      --Rollback if there was an error
+
      IF @@TRANCOUNT > 0
           ROLLBACK TRAN GIAPAPPROVERS;
 
